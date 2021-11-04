5ac53e83187e23e191020caef46a90d1ad1d1f19
srozemuller@unica.nl <srozemuller@uniccloud.nl>
2021-11-04 12:44:15 +0100

    with file

---
+++ trigger-terraform-run.ps1
@@ -0,0 +1,53 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$workspaceId,
+
+    [Parameter()]
+    [string]$token, 
+
+    [Parameter()]
+    [string]$triggerMessage
+)
+
+$body = @{
+    data = @{
+        attributes    = @{
+            message      = $triggerMessage
+            "auto-apply" = $true
+        }
+        type          = "runs"
+        relationships = @{
+            workspace = @{
+                data = @{
+                    type = "workspaces"
+                    id   = $workspaceId
+                }
+            }
+        }
+    }
+}
+$jsonBody = $body | ConvertTo-Json -Depth 5
+$token = $env:terraFormToken
+$header = @{
+    Authorization  = "Bearer $token"
+    "Content-Type" = "application/vnd.api+json"
+}
+$parameters = @{
+    header = $header
+    method = "POST"
+    body   = $jsonBody
+}
+# Start to run the workspace
+$runUrl = "https://app.terraform.io/api/v2/runs/"
+$runData = (Invoke-RestMethod -Uri $runUrl @parameters).data
+
+
+# Check for current run status
+$runDataUrl = $runUrl+ $runData.id
+$status = (Invoke-RestMethod -Uri $runDataUrl -Headers $header -Method GET).data.attributes.status
+do {
+    $status = (Invoke-RestMethod -Uri $runDataUrl -Headers $header -Method GET).data.attributes.status
+    $status
+}
+while ($status -ne 'applied')
